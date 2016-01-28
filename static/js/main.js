9599043559901104d12b5a1e935ca75363555f54
dsteinel <dimitri.steinel@gmail.com>
2016-01-28 08:30:23 +0100

    dont know, just pushed all

--- static/js/main.js
+++ static/js/main.js
@@ -1,53 +1,54 @@
-$(document).ready(function(){ 
-  colorize();
+$(document).ready(function(){
+	colorize();
 
-  var 
-    hejTouchingOffset = $(".hej-touching").offset().top
-    headHeight = $(".header").height()
-    imageHejHeight = $(".hej-touching").height()
-   ;
+	var
+		hejTouchingOffset = $(".hej-touching").offset().top,
+		headHeight = $(".header").height(),
+		imageHejHeight = $(".hej-touching").height(),
+		headerImage = $('.header').css('background-image'),
+		descriptionImages = $('.hej-description-steps').find('img'),
+		descriptionImageHeight = $(descriptionImages[0]).height()
+	 ;
 
-  $(window).scroll(function() {
-    if ($(window).scrollTop() > (headHeight)/2) {
-      $(".header").fadeOut();
-    } else {
-      $(".header").fadeIn();
-    }
-    if($(window).scrollTop() < hejTouchingOffset-(imageHejHeight)/1.5) {
-        $(".hej-touching").fadeOut("fast", function(){
-          $(".hej-no-touching").fadeIn("fast");
-        });
-    } else {
-      $(".hej-no-touching").fadeOut("fast", function(){
-        $(".hej-touching").fadeIn("fast");
-      });
-    }
-  });
+	$(window).scroll(function() {
+		if ($(window).scrollTop() > (headHeight)/2) {
+			$(".header").css('marginTop', '-200px');
+		} else {
+			$(".header").css('marginTop', '0');
+		}
+
+		if($(window).scrollTop() > ($(descriptionImages[0]).offset().top) - (descriptionImageHeight)*3 ) {
+			$(descriptionImages[0]).css('left', '0');
+		} if($(window).scrollTop() > ($(descriptionImages[1]).offset().top) - (descriptionImageHeight)*3 ) {
+			$(descriptionImages[1]).css('left', '0');
+		} if($(window).scrollTop() > ($(descriptionImages[2]).offset().top) - (descriptionImageHeight)*3 ) {
+			$(descriptionImages[2]).css('left', '0');
+		}
+	});
 });
 
 
 function colorize() {
-  //** notice we are including jquery and the color plugin at 
-  //** http://code.jquery.com/color/jquery.color-2.1.0.js
-  var scroll_pos = 0;
-  var animation_begin_pos = 0; //where you want the animation to begin
-  var animation_end_pos = 3000; //where you want the animation to stop
-  var beginning_color = new $.Color( 'rgb(210,50,98)' ); //we can set this here, but it'd probably be better to get it from the CSS; for the example we're setting it here.
-  var ending_color = new $.Color( 'rgb(0,197,209)' ); ;//what color we want to use in the end
-  $(document).scroll(function() {
-      scroll_pos = $(this).scrollTop(); 
-      if(scroll_pos >= animation_begin_pos && scroll_pos <= animation_end_pos ) { 
-          var percentScrolled = scroll_pos / ( animation_end_pos - animation_begin_pos );
-          var newRed = beginning_color.red() + ( ( ending_color.red() - beginning_color.red() ) * percentScrolled );
-          var newGreen = beginning_color.green() + ( ( ending_color.green() - beginning_color.green() ) * percentScrolled );
-          var newBlue = beginning_color.blue() + ( ( ending_color.blue() - beginning_color.blue() ) * percentScrolled );
-          var newColor = new $.Color( newRed, newGreen, newBlue );
-          //console.log( newColor.red(), newColor.green(), newColor.blue() );
-          $('html').animate({ backgroundColor: newColor }, 0);
-      } else if ( scroll_pos > animation_end_pos ) {
-           $('body').animate({ backgroundColor: ending_color }, 0);
-      } else if ( scroll_pos < animation_begin_pos ) {
-           $('body').animate({ backgroundColor: beginning_color }, 0);
-      } else { }
-  });
+	//** notice we are including jquery and the color plugin at
+	//** http://code.jquery.com/color/jquery.color-2.1.0.js
+	var scroll_pos = 0;
+	var animation_begin_pos = 0;
+	var animation_end_pos = 10000;
+	var beginning_color = new $.Color( 'rgb(110,90,98)' );
+	var ending_color = new $.Color( 'rgb(0,197,209)' ); ;
+	$(document).scroll(function() {
+			scroll_pos = $(this).scrollTop();
+			if(scroll_pos >= animation_begin_pos && scroll_pos <= animation_end_pos ) {
+					var percentScrolled = scroll_pos / ( animation_end_pos - animation_begin_pos );
+					var newRed = beginning_color.red() + ( ( ending_color.red() - beginning_color.red() ) * percentScrolled );
+					var newGreen = beginning_color.green() + ( ( ending_color.green() - beginning_color.green() ) * percentScrolled );
+					var newBlue = beginning_color.blue() + ( ( ending_color.blue() - beginning_color.blue() ) * percentScrolled );
+					var newColor = new $.Color( newRed, newGreen, newBlue );
+					$('html').animate({ backgroundColor: newColor }, 0);
+			} else if ( scroll_pos > animation_end_pos ) {
+					 $('body').animate({ backgroundColor: ending_color }, 0);
+			} else if ( scroll_pos < animation_begin_pos ) {
+					 $('body').animate({ backgroundColor: beginning_color }, 0);
+			} else { }
+	});
 }
